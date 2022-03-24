e67b44192a4f623d4a18660b18a8ec491f8cbdab
Alexdesi <alexdesi@gmail.com>
2022-03-24 13:01:21 +0000

    Add scenario simulations for Search and ApiQuotasSimulation.

---
+++ src/test/scala/tradetariff/SearchSimulation.scala
@@ -0,0 +1,31 @@
+package tradetariff
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
+
+import scala.concurrent.duration._
+
+class SearchSimulation extends Simulation {
+
+  val httpProtocol: HttpProtocolBuilder = http
+    .baseUrl("https://tariff-frontend-staging.london.cloudapps.digital")
+
+  val searchQueries = csv("search_queries.csv").random
+
+  val request = feed(searchQueries)
+    .exec(
+      http("Search")
+        .get("/search?q=${query}")
+    ).pause(1)
+
+  val searchScenario = scenario("Search").exec(request)
+
+  setUp(
+    searchScenario.inject(
+      constantConcurrentUsers(1).during(10.seconds), // 1
+      rampConcurrentUsers(1).to(100).during(60.seconds)
+    )
+  ).protocols(httpProtocol)
+}
