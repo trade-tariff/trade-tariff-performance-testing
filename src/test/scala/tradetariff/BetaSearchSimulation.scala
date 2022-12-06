681ebbd340068807195790e870056c0e4d832c29
William Fish <william.michael.fish@gmail.com>
2022-12-06 14:17:08 +0000

    HOTT0-2149: Move to escaping query params with method

--- src/test/scala/tradetariff/BetaSearchSimulation.scala
+++ src/test/scala/tradetariff/BetaSearchSimulation.scala
@@ -17,7 +17,8 @@ class BetaSearchSimulation extends Simulation {
   val request = feed(searchQueries)
     .exec(
       http("BetaSearch ${query}")
-        .get("/uk/api/beta/search?q=${query}")
+        .get("/uk/api/beta/search")
+        .queryParam("q", "${query}")
         .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15")
         .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
         .header("Accept-Encoding", "gzip, deflate, br")
